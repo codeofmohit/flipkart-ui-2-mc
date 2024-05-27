1af2d0f60482bce684a1589a8bc1183b21b3a48f
codeofmohit <mohit156@gmail.com>
2024-05-27 17:07:01 +0530

    filter logic updated

--- flipkart-chat-app/src/components/ChatList.jsx
+++ flipkart-chat-app/src/components/ChatList.jsx
@@ -5,6 +5,8 @@ import ChatItem from "./ChatItem";
 const ChatList = ({ messageList, setMessageList }) => {
   // local state for holding data
   const [data, setData] = useState(null);
+  // local state to store filter term
+  const [filterTerm, setFilterTerm] = useState("");
 
   // making api call via given api endpoin to fetch the data
   useEffect(() => {
@@ -27,22 +29,56 @@ const ChatList = ({ messageList, setMessageList }) => {
     return;
   }
 
+  const filterResults = (e) => {
+    const filterTerm = e.target.value;
+
+    setFilterTerm(filterTerm);
+    const filteredData = data.filter((each) => {
+      return (
+        each.title.toLowerCase().indexOf(filterTerm.toLowerCase()) !== -1 ||
+        each.orderId.toLowerCase().indexOf(filterTerm.toLowerCase()) !== -1
+      );
+    });
+    if (filterTerm === "") {
+      setData(data);
+    } else {
+      setData(filteredData);
+    }
+  };
+
+  console.log(data);
+
   return (
-    <div className="chatList">
-      {data.map((each) => {
-        const { title, imageURL, orderId, messageList } = each;
-        return (
-          <ChatItem
-            key={each.id}
-            setMessageList={setMessageList}
-            messageListState={messageList}
-            title={title}
-            imageURL={imageURL}
-            orderId={orderId}
-            messageList={messageList}
-          />
-        );
-      })}
+    <div className="chatListBlock">
+      <div className="filterList">
+        <input
+          type="text"
+          placeholder="search by title"
+          value={filterTerm}
+          // onChange={(e) => {
+          //   console.log(e.target.value);
+          //   setFilterTerm(e.target.value);
+          //   filterResults();
+          // }}
+          onChange={filterResults}
+        />
+      </div>
+      <div className="chatList">
+        {data.map((each) => {
+          const { title, imageURL, orderId, messageList } = each;
+          return (
+            <ChatItem
+              key={each.id}
+              setMessageList={setMessageList}
+              messageListState={messageList}
+              title={title}
+              imageURL={imageURL}
+              orderId={orderId}
+              messageList={messageList}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
